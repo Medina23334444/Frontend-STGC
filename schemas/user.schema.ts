51f7b977bc144a88e1d2972d9097722c93192600
Medina23334444 <luis.a.medina.c@unl.edu.ec>
2026-05-24 19:27:11 -0500

    Fix:Corregir el campos de user de scheme de vacio a null

--- schemas/user.schema.ts
+++ schemas/user.schema.ts
@@ -35,20 +35,28 @@ export const registerSchema = z.object({
   first_name: z.string()
     .min(3, 'Si lo ingresas, debe tener al menos 3 letras.')
     .regex(/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/, 'Solo se permiten letras.')
-    .or(z.literal('')), 
+    .or(z.literal('').transform(() => null)) 
+    .nullable()
+    .optional(),
 
   last_name: z.string()
     .min(3, 'Si lo ingresas, debe tener al menos 3 letras.')
     .regex(/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/, 'Solo se permiten letras.')
-    .or(z.literal('')), 
-
+    .or(z.literal('').transform(() => null)) 
+    .nullable()
+    .optional(),
+    
   identifier: z.string()
     .regex(/^\d{10}$/, 'La cédula debe tener exactamente 10 números.')
-    .or(z.literal('')), 
+    .or(z.literal('').transform(() => null)) 
+    .nullable()
+    .optional(),
 
   phone_number: z.string()
     .regex(/^09\d{8}$/, 'Debe ser un celular válido (ej: 0999999999).')
-    .or(z.literal('')), 
+    .or(z.literal('').transform(() => null)) 
+    .nullable()
+    .optional(),
 });
 
 export type RegisterFormInputs = z.infer<typeof registerSchema>;
